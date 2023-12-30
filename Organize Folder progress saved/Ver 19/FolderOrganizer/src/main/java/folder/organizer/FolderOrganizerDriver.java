9df8af907a4f0ce1ddb9c769f96309187dc6f731
Jawad Kazem Alyousef <78337911+JawadKazemAlyousef@users.noreply.github.com>
2023-12-30 03:59:35 +0300

    Update saved progress

--- Organize Folder progress saved/Ver 19/FolderOrganizer/src/main/java/folder/organizer/FolderOrganizerDriver.java
+++ Organize Folder progress saved/Ver 19/FolderOrganizer/src/main/java/folder/organizer/FolderOrganizerDriver.java
@@ -1,5 +1,4 @@
 package folder.organizer;
-<<<<<<< HEAD
 
 
 public class FolderOrganizerDriver{
@@ -9,24 +8,6 @@ public class FolderOrganizerDriver{
 		FolderOrganizer.main(args);
        
     }
-=======
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class FolderOrganizerDriver extends Application{
-
-
-	public static void main(String[] args) {
-		launch(args);
-       
-    }
-
-	@Override
-	public void start(Stage arg0) throws Exception {
-			OrganizeFolderMainUI mainObj = new OrganizeFolderMainUI();
-			mainObj.MainUI(mainObj, false);
-	}
->>>>>>> refs/heads/master
     
     
 
